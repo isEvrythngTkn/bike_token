a4bd734f9328227dd32aaa5a9b0f5efb633cdc55
IsEvrythngTkn <paulngoertzen@gmail.com>
2018-09-24 11:34:55 -0700

    adds some communication faculties to the bike contract

--- contracts/BikeCoin.sol
+++ contracts/BikeCoin.sol
@@ -1,9 +1,18 @@
 pragma solidity ^0.4.24;
 
 import '../node_modules/openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
+import './TokenRecipient.sol';
 
 contract BikeCoin is MintableToken {
   string public name = "Bike Coin";
   string public symbol = "BIKE";
   uint8 public decimals = 19;
+
+  function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {
+    TokenRecipient spender = TokenRecipient(_spender);
+    if (approve(_spender, _value)) {
+      spender.receiveApproval(msg.sender, _value, this, _extraData);
+      return true;
+    }
+  }
 }
